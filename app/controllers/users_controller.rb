9c8c062505fb9b04a93303889803d025e76b7cdb
sureeebabu <sureshkumar20141@gmail.com>
2020-04-24 17:09:52 +0530

    navigation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    
     @currentTime = Time.now
+  end
+  def addedituser
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   get 'users/index'
+  get 'users/addedituser'
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
